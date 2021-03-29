d04b797a7b53bda8135ee46d74bf32a9248d661a
pandurisathwik <74193944+pandurisathwik@users.noreply.github.com>
2021-03-29 12:44:41 +0530

    Add files via upload

---
+++ ADV-C89-student/guess_the_word_game/game_login.js
@@ -0,0 +1,9 @@
+function addUser(){
+   p1=document.getElementById("player1_name").value;
+   p2=document.getElementById("player2_name").value;
+
+   localStorage.setItem("player1",p1);
+   localStorage.setItem("player2",p2);
+
+   window.location="";
+}
